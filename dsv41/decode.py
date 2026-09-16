5894bd238de59c71d308b4bc98c6155869fcdfe8
shi3z <shi3z@me.com>
2026-09-16 08:54:33 +0000

    only grow persistent index owners in EP attention

--- dsv41/decode.py
+++ dsv41/decode.py
@@ -116,12 +116,13 @@ class DecodeRuntime:
                 cache = self.m.shared.compress_kv[(A.layer_id, d)]
                 row = torch.where(should, compress_len - 1, torch.full_like(compress_len, cache.shape[1] - 1))  # [B]
             if A.is_index_source:
-                self.m.shared._ensure_capacity(
-                    self.m.shared.index_k,
-                    A.layer_id,
-                    int(compress_len.max().item()),
-                    "index_k",
-                )
+                if getattr(A.indexer, "owns_k", False):
+                    self.m.shared._ensure_capacity(
+                        self.m.shared.index_k,
+                        A.layer_id,
+                        int(compress_len.max().item()),
+                        "index_k",
+                    )
                 idxs = self.indexer(A, x3, qr.view(B, 1, -1), latent, pos, compress_len, d, row if A.is_kv_source else None)
                 self.topk_buf[d].copy_(idxs)
             else:
